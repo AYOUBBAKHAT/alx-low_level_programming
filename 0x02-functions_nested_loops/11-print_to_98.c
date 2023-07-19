d50f2c9405206419faf6762193520834c9127683
root <bakhat.ayoubbusinnes@gmail.cm>
2023-07-18 19:49:15 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * print_to_ 98 - Prints all natural numbers from n to 98
+ * print_to_98 - Prints all natural numbers from n to 98
  * @n: The number to start printing from
- * Return: always 0.
+ * Return: Always 0.
  */
 
 void print_to_98(int n)
