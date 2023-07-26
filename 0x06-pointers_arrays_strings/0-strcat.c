bbf23a5211e24f94a76ab5a8dabcf9e24ebc1218
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2023-07-26 12:06:06 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,10 +5,25 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
+ * Return: destination string (char *ptr)
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+char *_strcat(char *dest, char *src)
 {
-	return (write(1, &c, 1));
+	int len1, len2, i;
+
+	for (len1 = 0; dest[len1] != '\n'; len1++)
+	{
+
+	}
+
+	for (len2 = 0; src[len2] != '\n'; len2++)
+	{
+
+	}
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len1 + i] = src[i];
+	}
+	return (dest);
 }
