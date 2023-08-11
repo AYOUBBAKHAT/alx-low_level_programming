5e1bc45edf252f7db580d2d7d4f46c21f0ad8a00
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2023-08-10 19:25:37 -0700

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -76,8 +76,7 @@ int main(int argc, char *argv[])
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-			{
+
 				digit2 = s2[len2] - '0';
 				carry += result[len1 + len2 + 1] + (digit1 * digit2);
 				result[len1 + len2 + 1] = carry % 10;
@@ -98,5 +97,4 @@ int main(int argc, char *argv[])
 		_putchar('\n');
 		free(result);
 		return (0);
-	}
 }
