dea6b072e8142e9a0dbc324efbfaae5a01292234
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2023-12-11 12:50:49 -0800

    python

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -p /tmp https: //github.com/AYOUBBAKHAT/alx-low_level_programming/raw/master/0x18_dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -p /tmp https: //github.com/AYOUBBAKHAT/alx-low_level_programming/raw/master/0x18_dynamic_libraries/libgiga.so export LD_PRELOAD=/tmp/libgiga.so
